b049c32d6a45162f196d47667afcee9d367e92dc
Antoine CLARMAN <anclarma@e1r9p12.42.fr>
2021-08-14 02:21:48 +0200

    correction mineur

--- libft/srcs/ft_memmove.c
+++ libft/srcs/ft_memmove.c
@@ -6,18 +6,22 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 04:25:58 by anclarma          #+#    #+#             */
-/*   Updated: 2021/08/13 04:35:02 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/08/14 02:15:37 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-void	*ft_memset(void *b, int c, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
 	i = -1;
-	while (++i < len)
-		*((unsigned char *)b + i) = (unsigned char)c;
-	return (b);
+	if (dst < src)
+		while (++i < len)
+			*((unsigned char *)dst + i) = *((unsigned char *)src + i);
+	else if (src < dst)
+		while (0 < len--)
+			*((unsigned char *)dst + len) = *((unsigned char *)src + len);
+	return (dst);
 }
