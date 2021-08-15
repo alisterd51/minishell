69906782e2c6b0336fbbae22dc6a8ac3f0266657
Antoine CLARMAN <anclarma@e1r8p9.42.fr>
2021-08-15 02:28:06 +0200

    ajout builtin pwd (non testee)

--- srcs/ft_pwd.c
+++ srcs/ft_pwd.c
@@ -6,13 +6,70 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/14 18:08:03 by anclarma          #+#    #+#             */
-/*   Updated: 2021/08/15 00:30:43 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/08/15 02:21:40 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/errno.h>
+#include "libft.h"
+
+static int	valid_option(const char *option, char *arg)
+{
+	if (arg[0] != '-' || arg[1] == '\0')
+		return (0);
+	arg++;
+	while (*arg)
+	{
+		if (!ft_strchr(arg, *option))
+			return (0);
+		arg++;
+	}
+	return (1);
+}
+
+static char	*alloc_pwd(void)
+{
+	char	*buf;
+	size_t	size;
+
+	size = 1;
+	buf = (char *)malloc(size);
+	if (!buf)
+		return (NULL);
+	while (!getcwd(buf, size))
+	{
+		if (errno != ERANGE)
+		{
+			free(buf);
+			return (NULL);
+		}
+		else if (size == 0)
+		{
+			free(buf);
+			return (NULL);
+		}
+		size *= 2;
+		buf = (char *)ft_realloc(buf, size);
+		if (!buf)
+			return (NULL);
+	}
+	return (buf);
+}
+
 int	ft_pwd(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	char	*buf;
+
+	while (ac > 0 && valid_option("LP", *av))
+	{
+		ac--;
+		av++;
+	}
+	buf = alloc_pwd();
+	ft_putendl_fd(buf, 1);
+	free(buf);
+	//if errno, affichage msg
 	return (0);
 }
