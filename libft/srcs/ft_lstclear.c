efe42d1bf55cc4db84cedca092f599b9aae7af0a
Antoine CLARMAN <anclarma@e1r1p7.42.fr>
2021-08-21 21:16:34 +0200

    correction erreur gestion list

--- libft/srcs/ft_lstclear.c
+++ libft/srcs/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 02:48:49 by anclarma          #+#    #+#             */
-/*   Updated: 2021/08/16 03:06:17 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/08/21 21:16:23 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*element;
+	t_list	*current;
 
-	while (*lst)
+	current = *lst;
+	while (current)
 	{
-		element = *lst;
-		lst = &((*lst)->next);
+		element = current;
+		current = current->next;
 		ft_lstdelone(element, del);
 	}
 	*lst = NULL;
