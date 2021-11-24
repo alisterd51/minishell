18fe634bc41e36ea0b4059f9c6811bca43233f52
Antoine CLARMAN <anclarma@e2r2p23.clusters.42paris.fr>
2021-11-24 21:13:39 +0100

    correction tests

--- test/main_ast.c
+++ test/main_ast.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 22:23:41 by anclarma          #+#    #+#             */
-/*   Updated: 2021/10/28 22:24:11 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/11/24 21:11:48 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,14 @@
 int	main(int ac, char **av, char **env)
 {
 	t_ast	*ast;
+	t_list	*lst_env;
 	int		ret;
 
 	ast = init_ast(ac - 1, av + 1);
 	print_ast(ast, 0);
-	ret = exec_ast(ast, env);
+	lst_env = init_env(env);
+	ret = exec_ast(ast, &lst_env);
+	clean_env(&lst_env);
 	clean_ast(&ast);
 	return (ret);
 }
