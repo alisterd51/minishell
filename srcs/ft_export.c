a08d11fb82e4709bffd226a174330bff0387363b
Antoine CLARMAN <anclarma@e2r2p23.clusters.42paris.fr>
2021-11-24 21:16:16 +0100

    norm

--- srcs/ft_export.c
+++ srcs/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 14:26:01 by anclarma          #+#    #+#             */
-/*   Updated: 2021/11/24 16:46:25 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/11/24 21:16:00 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,9 @@
 
 static int	is_special_param(char *param)
 {
-	return (*param == '*'
-			|| *param == '*'
-			|| *param == '@'
-			|| *param == '#'
-			|| *param == '?'
-			|| *param == '-'
-			|| *param == '$'
-			|| *param == '!'
-			|| ft_isdigit(*param));
+	return (*param == '*' || *param == '*' || *param == '@' || *param == '#'
+		|| *param == '?' || *param == '-' || *param == '$' || *param == '!'
+		|| ft_isdigit(*param));
 }
 
 int	ft_export(int ac, char **av, t_list **env)
@@ -35,7 +29,7 @@ int	ft_export(int ac, char **av, t_list **env)
 	while (*av)
 	{
 		if (is_special_param(*av))
-			ft_putendl_fd("bash: export: `10=a': not a valid identifier", 2);//
+			ft_putendl_fd("bash: export: `10=a': not a valid identifier", 2);
 		else
 			ft_lstadd_back(env, ft_lstnew(ft_strdup(*av)));
 		av++;
