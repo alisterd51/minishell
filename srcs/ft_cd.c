d0989447cbd9f4e15ab6abfddee2a85f2a8c7020
anclarma <anclarma@student.42.fr>
2021-12-21 16:17:14 +0100

    correction valgrind

--- srcs/ft_cd.c
+++ srcs/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 03:52:18 by anclarma          #+#    #+#             */
-/*   Updated: 2021/11/26 23:57:05 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/12/21 15:23:22 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	ft_cd(int ac, char **av, t_list **env)
 	int	ret;
 
 	(void)env;
+	ret = 0;
 	if (ac > 2)
 	{
 		ft_putendl_fd("cd: trop d'arguments", 2);
@@ -25,7 +26,7 @@ int	ft_cd(int ac, char **av, t_list **env)
 	}
 	else if (ac == 2)
 		ret = chdir(av[1]);
-	else
-		ret = chdir("/Users/anclarma");
+	if (ret != 0)
+		ret = 1;
 	return (ret);
 }
