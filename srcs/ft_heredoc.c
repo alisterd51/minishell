549ce333c472ec8dbd695d6b445476d0b7e98bc2
anclarma <anclarma@student.42.fr>
2022-02-04 03:42:29 +0100

    ajout readline

--- srcs/ft_heredoc.c
+++ srcs/ft_heredoc.c
@@ -6,27 +6,26 @@
 /*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 00:46:40 by anclarma          #+#    #+#             */
-/*   Updated: 2022/01/29 14:41:21 by anclarma         ###   ########.fr       */
+/*   Updated: 2022/02/04 03:42:10 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
-#include "libft.h"
+#include <readline/readline.h>
+#include "minishell.h"
 
 int	ft_heredoc(int fd, char const *terminat)
 {
 	char	*line;
 
-	line = NULL;
-	while (get_next_line(STDIN_FILENO, &line) > 0 && ft_strcmp(terminat, line))
+	line = readline(DEFAULT_PS2);
+	while (line && ft_strcmp(terminat, line))
 	{
 		write(fd, line, ft_strlen(line));
 		write(fd, "\n", 1);
 		free(line);
-		line = NULL;
+		line = readline(DEFAULT_PS2);
 	}
-	free(line);
-	line = NULL;
 	return (0);
 }
