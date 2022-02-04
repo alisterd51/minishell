105552acc5b3e11332699f0365cd8df5239f42af
anclarma <anclarma@student.42.fr>
2022-02-04 03:01:41 +0100

    ajout test

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,6 +5,7 @@ RESET="\e[0m"
 LIGHT_RED="\e[91m"
 LIGHT_GREEN="\e[92m"
 LIGHT_CYAN="\e[96m"
+VALGRIND="valgrind --read-var-info=yes --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes --suppressions=minimal.supp"
 
 test_default_main()
 {
@@ -31,7 +32,6 @@ test_default_main()
 
 test_minishell()
 {
-	cd ..
 	make fclean debug
 	printf "${LIGHT_CYAN}${BOLD}minishell < cmd1${RESET}\n"
 	./minishell < ./test/cmd1
@@ -49,14 +49,48 @@ test_minishell()
 	./minishell < ./test/cmd7
 	printf "${LIGHT_CYAN}${BOLD}minishell < cmd8${RESET}\n"
 	./minishell < ./test/cmd8
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd9${RESET}\n"
+	./minishell < ./test/cmd9
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd10${RESET}\n"
+	./minishell < ./test/cmd10
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd11${RESET}\n"
+	./minishell < ./test/cmd11
+}
+
+test_valgrind_minishell()
+{
+	make fclean all
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd1${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd1
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd2${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd2
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd3${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd3
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd4${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd4
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd5${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd5
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd6${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd6
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd7${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd7
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd8${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd8
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd9${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd9
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd10${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd10
+	printf "${LIGHT_CYAN}${BOLD}minishell < cmd11${RESET}\n"
+	${VALGRIND} ./minishell < ./test/cmd11
 }
 
 main()
 {
 	cd $(dirname $0)
-
 	test_default_main
+	cd ..
 	test_minishell
+	test_valgrind_minishell
 	make fclean
 }
 
