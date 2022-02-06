891567ac7d776b71a14d621223531457aa197c75
anclarma <anclarma@student.42.fr>
2022-02-06 09:09:48 +0100

    commentaire en moins

---
+++ srcs/expend_utils2.c
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expend-utils2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/06 08:51:30 by anclarma          #+#    #+#             */
+/*   Updated: 2022/02/06 09:03:42 by anclarma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lzac_pars1.h"
+#include "minishell.h"
+
+char	*search_env(char **envp, char *word)
+{
+	int		i;
+	char	*str;
+	char	*key;
+
+	i = 0;
+	str = NULL;
+	if (!envp)
+		return (NULL);
+	key = ft_strjoin(word, "=");
+	free(word);
+	while (envp[i] && ft_strncmp(envp[i], key, ft_strlen(key)) != 0)
+		i++;
+	if (envp[i] && (ft_strncmp(envp[i], key, ft_strlen(key)) == 0))
+	{
+		str = envp[i];
+		str = str + ft_strlen(key);
+		free(key);
+		return (ft_strdup(str));
+	}
+	free(key);
+	return (NULL);
+}
+
+void	ft_expend(t_stuff *stuff, char **envp, t_cmd **cmd)
+{
+	t_cmd	*new;
+	char	*word;
+	int		j;
+
+	j = 0;
+	stuff->i++;
+	stuff->type = ARGUMENT;
+	j = ft_expstrclen(stuff->str + stuff->i, ' ');
+	if (j == 0)
+	{
+		word = "$";
+		stuff->type = DOLLAR;
+	}
+	else
+	{
+		word = ft_strndup(stuff->str + stuff->i, j);
+		if (word && word[0] == '?')
+			word = ft_itoa(ft_get_status());
+		else
+			word = search_env(envp, word);
+		if (word == NULL)
+		{
+			stuff->i++;
+			free(word);
+			stuff->i += j - 1;
+			return ;
+		}
+	}
+	new = lzac_ft_lstnew(word, stuff->type, stuff->space);
+	lzac_ft_lstadd_back(cmd, new);
+	stuff->i += j;
+}
+
+char	*ft_expend_quote(char *word, int *i, char **envp)
+{
+	int		j;
+
+	j = 0;
+	(*i)++;
+	j = ft_expstrclen(word + *i, ' ');
+	word = ft_strndup(word + *i, j);
+	if (word && word[0] == '?')
+		word = ft_itoa(ft_get_status());
+	else
+		word = search_env(envp, word);
+	if (word == NULL)
+	{
+		(*i)++;
+		free(word);
+		(*i) += j - 1;
+		return (NULL);
+	}
+	(*i) += j;
+	return (word);
+}
+
+int	ft_cmdsize(t_cmd *lst)
+{
+	int	ret;
+
+	ret = 0;
+	while (lst)
+	{
+		ret++;
+		lst = lst->next;
+	}
+	return (ret);
+}
+
+t_cmd	*ft_cmdlast(t_cmd *lst)
+{
+	while (lst && lst->next)
+		lst = lst->next;
+	return (lst);
+}
